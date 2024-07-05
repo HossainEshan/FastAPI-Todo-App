4593afe52d0ab700c93b76a9edadde3a7b545c5e
Hossain Eshan <eshan.shahriar.77@gmail.com>
2024-07-05 22:07:14 +0600

    Modularized routers, begin auth router

--- main.py
+++ main.py
@@ -1,62 +1,13 @@
-from api.database.dbconfig import engine, SessionLocal
-from api.database.models import Base, Todos
-from fastapi import FastAPI, Depends, HTTPException, Path
-from sqlalchemy.orm import Session
-from typing import Annotated
-from pydantic import BaseModel, Field
-from starlette import status
+from fastapi import FastAPI
+from api.database.dbconfig import engine
+from api.database.models import Base
+from api.routers import todos, auth
 
 # Initialize the app
 app = FastAPI()
 
+app.include_router(todos.router)
+app.include_router(auth.router)
+
 # Create the database dynamically
 Base.metadata.create_all(bind=engine)
-
-# Routes -----------------------------------------------
-def get_db():
-    db = SessionLocal()
-    try:
-        yield db
-    finally:
-        db.close()
-
-db_dependency = Annotated[Session, Depends(get_db)]
-
-class TodoRequest(BaseModel):
-    title: str = Field(min_length=3)
-    description: str = Field(min_length=3, max_length=100)
-    priority: int = Field(gt=0, lt=6)
-    complete: bool
-
-@app.get('/todos', status_code=status.HTTP_200_OK)
-async def read_all(db: db_dependency):
-    return db.query(Todos).all()
-
-@app.get('/todos/{todo_id}', status_code=status.HTTP_200_OK)
-async def read_by_id(db: db_dependency, todo_id: int = Path(gt=0)):
-    result = db.query(Todos).filter(Todos.id == todo_id).first()
-    if not result:
-        raise HTTPException(status_code=404, detail='Todo not found')
-    return result
-    
-
-@app.post('/todos', status_code=status.HTTP_201_CREATED)
-async def create_todo(db: db_dependency, todo_request: TodoRequest):
-    todo_model = Todos(**todo_request.model_dump())
-    
-    db.add(todo_model)
-    db.commit()
-    
-@app.put('/todos/{todo_id}', status_code=status.HTTP_204_NO_CONTENT)
-async def update_todo(db: db_dependency, todo_request: TodoRequest, todo_id: int = Path(gt=0)):
-    todo_model = db.query(Todos).filter(Todos.id == todo_id).first()
-    if not todo_model:
-        raise HTTPException(status_code=404, detail='Todo not found')
-    
-    todo_model.title = todo_request.title
-    todo_model.description = todo_request.description
-    todo_model.priority = todo_request.priority
-    todo_model.complete = todo_request.complete
-    
-    db.add(todo_model)
-    db.commit()
